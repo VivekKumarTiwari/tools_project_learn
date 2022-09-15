51c8fd71f6b24138a60dfe2ffef65591a4af1355
Vivek Tiwari <vivektiwari.tiwari125@gmail.com>
2022-09-15 21:50:44 +0530

    antbasic_java_example project

---
+++ antbasic_java_example/src/com/vivek/it/TestMain.java
@@ -0,0 +1,7 @@
+package com.vivek.it;
+public class TestMain{
+
+    public static void main(String[] args) {
+        System.out.println("Hi guy's ! You are entered in the Vivek IT group!!");
+    } 
+}
